229ba7ccd0631a0fd433056541f21ae9246157cf
METALZILO <adrianosilvaoliveiradejesus@gmail.com>
2019-06-12 15:49:42 -0300

    Initial Commit

---
+++ ambiente controlado/cnc/copiador.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+src = os.getcwd()
+print(src)
+
+src_files = os.listdir(src)
+for file_name in src_files:
+ full_file_name = os.path.join(src, file_name)
+ if os.path.isfile(full_file_name):
+    shutil.copy(full_file_name, "/home/metalzilo/Área de Trabalho/ambiente controlado/cnc")
