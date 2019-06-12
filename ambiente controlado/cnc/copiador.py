9efb456d91c0266c61a894c6cde0d2fa141331f0
METALZILO <adrianosilvaoliveiradejesus@gmail.com>
2019-06-12 16:02:16 -0300

    second commit

--- ambiente controlado/cnc/copiador.py
+++ ambiente controlado/cnc/copiador.py
@@ -5,7 +5,8 @@ src = os.getcwd()
 print(src)
 
 src_files = os.listdir(src)
+print(src_files)
 for file_name in src_files:
  full_file_name = os.path.join(src, file_name)
  if os.path.isfile(full_file_name):
-    shutil.copy(full_file_name, "/home/metalzilo/Área de Trabalho/ambiente controlado/cnc")
+    shutil.copy(full_file_name, "/home/metalzilo/Área de Trabalho/Copiador-de-Arquivos/ambiente controlado/cnc")
--- ambiente controlado/copiador.py
+++ ambiente controlado/copiador.py
@@ -1,11 +1,12 @@
 import os
 import shutil
 
-src = "C:/cnc"
+src = os.getcwd()
 print(src)
 
 src_files = os.listdir(src)
+print(src_files)
 for file_name in src_files:
  full_file_name = os.path.join(src, file_name)
  if os.path.isfile(full_file_name):
-    shutil.copy(full_file_name, "/home/metalzilo/Área de Trabalho/ambiente controlado/cnc")
+    shutil.copy(full_file_name, "/home/metalzilo/Área de Trabalho/Copiador-de-Arquivos/ambiente controlado/cnc")
